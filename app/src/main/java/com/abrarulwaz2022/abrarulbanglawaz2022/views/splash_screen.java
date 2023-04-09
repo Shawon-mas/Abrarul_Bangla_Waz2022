9815ce6a73957b07c2c5e9569d3ea042edceffc0
Mahabub Alam Shawon <mahabub15-8580@diu.edu.bd>
2023-04-09 23:45:09 +0600

    updated on 2023

---
+++ app/src/main/java/com/abrarulwaz2022/abrarulbanglawaz2022/views/splash_screen.java
@@ -0,0 +1,16 @@
+package com.abrarulwaz2022.abrarulbanglawaz2022.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.abrarulwaz2022.abrarulbanglawaz2022.R;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
